b6482e2f800d4cfe4909adaec38f60e8d37de2bb
jlrdh <69487096+jlrdh@users.noreply.github.com>
2023-08-27 23:11:39 +0200

    Delete src directory

--- src/globals.h
+++
@@ -1,5 +0,0 @@
-//static int current_move[256];	// @ed
-//static int current_ply;			// @ed
-extern POSITIONDATA posdata[64][256];
-//static int current_captype[256];	// -1 if not a capture, else captype
-//static POSITIONDATA posdata[256];
